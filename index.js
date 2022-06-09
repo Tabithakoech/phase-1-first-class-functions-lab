d239689bf946a77de386fd6a5df330f20fc36550
TabithaKoech <tabbyk1718@gmail.com>
2022-06-09 20:27:09 +0300

    commit tree 6d879e3da10139874d297861f8cb8957a63c95fe

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers=function(){
+    return (drivers.slice(0,2))   
+}
+const returnLastTwoDrivers= function(){
+    return (drivers.slice (2,4))
+}
+const selectingDrivers = function(){
+    returnFirstTwoDrivers()
+    returnLastTwoDrivers()
+}
+
